2c1c2c1702a1c79c4bad7349e2f45a0f237a19b7
preet-singh <preetsingh13@gmail.com>
2019-09-23 16:32:54 -0500

    drill 3 complete

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.get('/sum', (req, res) => {
         return res.status(400).send('Number isn\'t valid, b must be a number');
     }
     const resText = `The sum of ${a} and ${b} = ${numberA + numberB}`;
-    res.send(resText);
+    res.status(200).send(resText);//added status 200 in after looking at solution
     //console.log(resText);
 });
 
@@ -32,6 +32,7 @@ app.get('/sum', (req, res) => {
 
 app.get('/cipher', (req, res) => {
     const { text, shift } = req.query;
+    //add validation here
     const base = 'A'.charCodeAt(0);
     const cipher = text.toUpperCase().split('').map(character => {
         const characterCode = character.charCodeAt(0); 
@@ -46,11 +47,48 @@ app.get('/cipher', (req, res) => {
         return shiftedCharacter;
     })
     const newCipher = cipher.join('');
-    res.send(newCipher);
+    res.status(200).send(newCipher);
 });
 
 //Drill 3
 
+app.get('/lotto', (req,res) => {
+    const arr = req.query.arr;
+    let results = 0;
+    const lotto = (() => {
+      let lottoArray =[];
+      for (let i=0;i<6;i++) {
+        lottoArray.push(Math.ceil(Math.random()*20))
+      }
+      return lottoArray; 
+    })();
+    
+    arr.forEach((num) => {
+      const index = lotto.findIndex(lottoNum => lottoNum === parseInt(num));
+      if (index !== -1) {
+        lotto.splice(index, 1, 'words');
+        results ++;
+      } 
+    })
+  
+    let resultsString = '';
+  
+    switch(results){
+      case 4:
+      resultsString = 'Congratulation you win a free ticket!'
+      break;
+      case 5:
+        resultsString = 'Congratulations! You win $100!'
+        break;
+      case 6:
+        resultsString = 'Wow! Unbelievable! You could have won the mega millions!'
+        break;
+      default:
+        resultsString = 'Sorry, you lose'
+        break;
+    }
+    res.send(resultsString);
+  })
 
 app.listen(8000, () => {
     console.log('Express server is listening on port 8000');
